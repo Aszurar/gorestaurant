3d67cec0e0215ad967bbf8da471b0c99fe791d60
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2025-04-28 16:17:28 -0300

    refactor: update error messages in messages.ts for clarity and consistency

--- src/utis/messages.ts
+++ src/utis/messages.ts
@@ -1,9 +1,9 @@
 export const MESSAGES = {
   ERROR: {
+    tryAgainLater: 'Tente novamente mais tarde',
     GET_FOODS: {
       title: 'Erro ao carregar os alimentos',
-      description:
-        'Ocorreu um erro ao carregar os alimentos. Tente novamente mais tarde.',
+      description: 'Ocorreu um erro ao carregar os alimentos.',
     },
   },
 }
